5c06ba9d425d18880ffec0356a424f82e5badce3
Anastasia-Lis-0411 <Lis0411anastasia@gmail.com>
2025-10-17 20:52:07 +0300

    fff

--- Assets/script/playerControler.cs
+++ Assets/script/playerControler.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class playerControler : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5f; //скорость перемещ
+    public float jumpForce = 5f;
+    private Rigidbody2D rb; // компонент rigidBody2d
+    public bool isGrounded; // находится ли персонаж на земле
+    private Animator anim;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Jump();
+
+
+        // проверка стоим ли мы на земле
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            anim.SetBool("jump",false);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+    private void Move()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horiz * moveSpeed, rb.velocity.y);
+        anim.SetBool("walk", horiz != 0);
+        //поворачиваем персонаж в сторону движения
+        if (horiz > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        if (horiz < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+    }
+
+    private void Jump()
+    {
+        // прыжок
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            anim.SetBool("jump", true);
+        }
     }
 }
